cb090ca4f47155f38e6a3d74b5673a6b7ba17e04
Hana Bae <baehana980@gmail.com>
2023-06-01 19:45:28 +0900

    테스트

--- FoodCode/src/main/java/com/itwillbs/foodcode/controller/StoreController.java
+++ FoodCode/src/main/java/com/itwillbs/foodcode/controller/StoreController.java
@@ -32,7 +32,7 @@ import com.mysql.cj.Session;
 
 @Controller
 public class StoreController {
-	
+	//테스트
 	 @Autowired
 	 private StoreService storeService;
 	 
@@ -81,67 +81,67 @@ public class StoreController {
 	
 
 	// 점주가 owner_store_register.jsp폼에서 새로운식당을 등록(가게등록)하는 Pro작업 - 4/9배하나수정
-		// 가게등록 폼의 첨부파일 작업 추가 - 4/13 배하나수정
-		@PostMapping(value = "/storeRegisterPro.so")
-		public String storeRegisterPro(StoreVO store, Model model, HttpSession session) {
-			System.out.println("storeRegisterPro");
-			System.out.println("테스트1123 : " + store);
-			System.out.println("로그인된 id값 : " + session.getAttribute("sId"));
+	// 가게등록 폼의 첨부파일 작업 추가 - 4/13 배하나수정
+	@PostMapping(value = "/storeRegisterPro.so")
+	public String storeRegisterPro(StoreVO store, Model model, HttpSession session) {
+		System.out.println("storeRegisterPro");
+		System.out.println("테스트1123 : " + store);
+		System.out.println("로그인된 id값 : " + session.getAttribute("sId"));
 //			System.out.println("업로드 파일명 : " + store.getStore_file().getOriginalFilename());
-			
-			
-			//---------- 파일 업로드 관련 작업 시작 -----------------------------------------------------------
-			String uploadDir = "/resources/storeFileUpload"; //프로젝트상의 가상 업로드 경로
-			String saveDir = session.getServletContext().getRealPath(uploadDir); //실제 업로드 경로
-			System.out.println("실제 업로드 경로 : " + saveDir);
+		
+		
+		//---------- 파일 업로드 관련 작업 시작 -----------------------------------------------------------
+		String uploadDir = "/resources/storeFileUpload"; //프로젝트상의 가상 업로드 경로
+		String saveDir = session.getServletContext().getRealPath(uploadDir); //실제 업로드 경로
+		System.out.println("실제 업로드 경로 : " + saveDir);
 //			실제 업로드 경로(resources뒤에 띄어쓰기 되어있음) : C:\Users\HANABAE\workspace_sts\.metadata\.plugins\org.eclipse.wst.server.core\tmp1\wtpwebapps\FoodCode\resourcees\ storeFileupload
-			try {
-				Date date = new Date(); //java.util.Date 클래스 사용하기
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-				store.setStore_file_path("/" + sdf.format(date));
-				
-				saveDir = saveDir + store.getStore_file_path(); //실제 업로드 경로와 서브 디렉토리 경로 결합하여 저장
-				
-				Path path = Paths.get(saveDir);
-				Files.createDirectories(path);
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		try {
+			Date date = new Date(); //java.util.Date 클래스 사용하기
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+			store.setStore_file_path("/" + sdf.format(date));
 			
-			MultipartFile mFile = store.getFile(); //StoreVO 객체에 전달된 MultipartFile 단일파일 꺼내기
-			String originalFileName = mFile.getOriginalFilename();
+			saveDir = saveDir + store.getStore_file_path(); //실제 업로드 경로와 서브 디렉토리 경로 결합하여 저장
 			
-			String uuid = UUID.randomUUID().toString(); //파일명 중복 방지를 위한 코드
-			System.out.println("UUID : " + uuid);
+			Path path = Paths.get(saveDir);
+			Files.createDirectories(path);
 			
-			store.setStore_file(uuid.substring(0, 8) + "_" + originalFileName);
-			System.out.println("실제 업로드 될 파일명 : " + store.getStore_file());
-			//---------- 파일 업로드 관련 작업 끝 ------------------------------------------------------------
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		MultipartFile mFile = store.getFile(); //StoreVO 객체에 전달된 MultipartFile 단일파일 꺼내기
+		String originalFileName = mFile.getOriginalFilename();
+		
+		String uuid = UUID.randomUUID().toString(); //파일명 중복 방지를 위한 코드
+		System.out.println("UUID : " + uuid);
+		
+		store.setStore_file(uuid.substring(0, 8) + "_" + originalFileName);
+		System.out.println("실제 업로드 될 파일명 : " + store.getStore_file());
+		//---------- 파일 업로드 관련 작업 끝 ------------------------------------------------------------
 
-			//--------------- 가게 INSERT작업에 관한 코드 시작 ------------------------------------------------
-			store.setMember_id(session.getAttribute("sId").toString()); //=> store테이블의 member_id컬럼에 저장할 점주의 sId 가져오기
-			int insertCnt = storeService.insertStore(store);
-			
-			  if(insertCnt > 0) {
-				  // 가게등록작업 성공 시 임시 위치에 저장된 파일을 실제 폴더로 옮기는 작업
-				  try {
-						mFile.transferTo(new File(saveDir, store.getStore_file()));
-					} catch (IllegalStateException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				  	model.addAttribute("msg", "완료되었습니다. 관리자 승인 후 맛집추천페이지에 자동으로 노출됩니다 :)");
-				  	model.addAttribute("target", "mypage.me"); // 가게등록 후 점주 본인의 마이페이지로 이동
-				  	return "success";
+		//--------------- 가게 INSERT작업에 관한 코드 시작 ------------------------------------------------
+		store.setMember_id(session.getAttribute("sId").toString()); //=> store테이블의 member_id컬럼에 저장할 점주의 sId 가져오기
+		int insertCnt = storeService.insertStore(store);
+		
+		  if(insertCnt > 0) {
+			  // 가게등록작업 성공 시 임시 위치에 저장된 파일을 실제 폴더로 옮기는 작업
+			  try {
+					mFile.transferTo(new File(saveDir, store.getStore_file()));
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			  	model.addAttribute("msg", "완료되었습니다. 관리자 승인 후 맛집추천페이지에 자동으로 노출됩니다 :)");
+			  	model.addAttribute("target", "mypage.me"); // 가게등록 후 점주 본인의 마이페이지로 이동
+			  	return "success";
 //		            return "redirect:/store_recommend.so";
-		      } else {
-		            model.addAttribute("msg", "가게 등록 실패!");
-		            return "fail_back";
-		      }
-			  
-		} //storeRegisterPro() 끝
+	      } else {
+	            model.addAttribute("msg", "가게 등록 실패!");
+	            return "fail_back";
+	      }
+		  
+	} //storeRegisterPro() 끝
 	
 	
 		// 가게등록한 정보를 storeList에 담아 맛집추천페이지에 뿌려줌 - 4/10 배하나수정
